2c2fc2d4f05eeb03adfc12d1538a8351735750d5
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-11-26 21:01:33 -0300

    Binary Search

---
+++ hello-world/demo/src/main/java/com/example/arrays/BSearch.java
@@ -0,0 +1,23 @@
+package com.example.arrays;
+
+public class BSearch {
+    public BSearch(){}
+
+    public int solution(int[] array, int target){
+        int left = 0;
+        int right = array.length - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2; // to prevent overflow
+            if(array[mid] == target){
+                return mid;
+            }
+            else if ( array[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1; // target not found
+
+    }
+}
